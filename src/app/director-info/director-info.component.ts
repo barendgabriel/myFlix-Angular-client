9ae096dd18f694385adf3a679e4412e4148930fd
barendgabriel <jansenvanrensburg.ben@gmail.com>
2025-02-07 12:45:25 +0100

    angular final submission

---
+++ src/app/director-info/director-info.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+/**
+ * @description Component of a dialog box displaying director info
+ * @selector: 'app-director-info'
+ * @templateUrl: './director-info.component.html'
+ * @styleUrls: ['./director-info.component.scss']
+ */
+@Component({
+  selector: 'app-director-info',
+  templateUrl: './director-info.component.html',
+  styleUrl: './director-info.component.scss',
+})
+export class DirectorInfoComponent implements OnInit {
+  /**
+   * Called when creating an instance of the class
+   * @constructor
+   * @param data pulled from the director object
+   */
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Name: string;
+      Bio: string;
+      Birth: string;
+      Death: string;
+    }
+  ) {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
---
+++ src/app/welcome-page/welcome-page.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
+
+/**
+ * Component displaying the welcome page
+ * @selector 'app-welcome-page'
+ * @templateUrl './welcome-page.component.html'
+ * @styleUrls ['./welcome-page.component.scss']
+ */
+@Component({
+  selector: 'app-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrls: ['./welcome-page.component.scss'],
+})
+export class WelcomePageComponent implements OnInit {
+  /**
+   * Called when creating an instance of the class
+   * @constructor
+   * @param dialog - references this component when opening the dialog
+   */
+  constructor(public dialog: MatDialog) {}
+  ngOnInit(): void {}
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px',
+    });
+  }
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px',
+    });
+  }
+}
